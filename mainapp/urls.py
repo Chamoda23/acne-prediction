bbc8980535a01c23c147dc477131382720f203f9
ChamodaTK <114578750+Chamoda23@users.noreply.github.com>
2023-04-30 20:40:56 +0530

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,5 +5,3 @@ urlpatterns = [
     path('image', views.ImageView.as_view(), name='image')
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
